2c9c86ab6c2e3d3aedd3c4943f1d5e15e6e50b12
DhruvBagga47 <dhruv.bagga47@gmail.com>
2025-01-11 17:10:52 -0500

    Authenticator created

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Auth0Provider
+    domain="dev-8ednezwqq36kf4ie.ca.auth0.com"
+    clientId="PPkqxCFIPfBhSwpkyOGV37o4EVnLG5vj"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </StrictMode>,
-)
+  </Auth0Provider>
+);
+
