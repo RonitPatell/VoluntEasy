c7b315dec4b9bbdf72f4d9643b3ce5949fdb1944
ManavP16 <patelvmanav@gmail.com>
2025-01-11 21:20:27 -0500

    tailwind changes!

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all React files
+    "./src/index.css",            // Include the custom CSS file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
